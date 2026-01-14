c3df64d53b67eb1ec2d80017e40f39d0c12b534d
Edgar Alan Rodriguez Amador <edgaralanra@gmail.com>
2026-01-14 00:42:44 -0600

    Refactor establecimientos fetch and add superadmin view
    Replaces legacy establecimientos fetch functions with new getStores and getStoreById for superadmin endpoints. Updates EstablecimientoDetalle to use getStoreById, removes unused code and logs, and integrates a new EstablecimientosSuperAdmin component for superadmin users in the Establecimientos page.

--- src/pages/establecimientos/Establecimientos.tsx
+++ src/pages/establecimientos/Establecimientos.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { EstablecimientosAdministradoresClients } from './EstablecimientosAdministradoresClients'
+import { EstablecimientosSuperAdmin } from './EstablecimientosSuperAdmin'
 
 import { useAuthStore } from '../../store/authStore';
 
@@ -13,7 +14,7 @@ const Establecimientos: React.FC = () => {
 
         { (user?.i_rol === 1) && (
           <>
-            <p>Establecimientos superadmin</p>
+            <EstablecimientosSuperAdmin />
           </>
         )}
 
